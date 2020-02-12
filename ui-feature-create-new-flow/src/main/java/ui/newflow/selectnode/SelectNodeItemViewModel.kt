e8fbb570a1e6eb27e84a4204557cacaccaa78ea5
PravinSonawane <june.pravin@gmail.com>
2020-02-11 21:22:54 -0800

    Made view type public
    Signed-off-by: PravinSonawane <june.pravin@gmail.com>

--- ui-feature-create-new-flow/src/main/java/ui/newflow/selectnode/SelectNodeItemViewModel.kt
+++ ui-feature-create-new-flow/src/main/java/ui/newflow/selectnode/SelectNodeItemViewModel.kt
@@ -4,7 +4,7 @@ import ui.feature.create_new_flow.BR
 import ui.feature.create_new_flow.R
 import ui.lib.views.ItemViewModel
 
-private const val VIEW_TYPE_ITEM_NODE = 100000
+const val VIEW_TYPE_ITEM_NODE = 100000
 
 class SelectNodeItemViewModel : ItemViewModel<SelectNodeItemViewModel.Event>(
     R.layout.list_item_select_node,
