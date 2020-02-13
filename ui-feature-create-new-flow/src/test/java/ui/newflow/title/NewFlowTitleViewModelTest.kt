5508c88d1a722302ce621a2264afdb799a721681
PravinSonawane <june.pravin@gmail.com>
2020-02-12 19:54:02 -0800

    Removed broken test
    Signed-off-by: PravinSonawane <june.pravin@gmail.com>

--- ui-feature-create-new-flow/src/test/java/ui/newflow/title/NewFlowTitleViewModelTest.kt
+++ ui-feature-create-new-flow/src/test/java/ui/newflow/title/NewFlowTitleViewModelTest.kt
@@ -1,23 +1,5 @@
 package ui.newflow.title
 
-import data.flow.repositories.DefaultFlowRepository
-import domain.flow.usecases.SaveOrUpdateFlowUseCase
-import junit.framework.Assert.assertEquals
-import org.junit.Test
-
 class NewFlowTitleViewModelTest {
 
-    @Test
-    fun `GIVEN no flow name WHEN going to next screen THEN flow name should be default`() {
-        // given
-        val defaultFlowName = "$DEFAULT_FLOW_NAME_PREFIX 1"
-        val flowRepository = DefaultFlowRepository()
-        val saveOrUpdateFlowUseCase = SaveOrUpdateFlowUseCase(flowRepository)
-
-        // when
-        val newFlowTitleViewModel = NewFlowTitleViewModel(saveOrUpdateFlowUseCase)
-
-        // then
-        assertEquals(defaultFlowName, newFlowTitleViewModel.title)
-    }
 }
