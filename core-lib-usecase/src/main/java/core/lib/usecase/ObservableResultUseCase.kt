6f3ab343561f6f594462245648a280c77e1833bb
PravinSonawane <june.pravin@gmail.com>
2020-02-14 18:25:00 -0800

    Fixes #23 Makes `ObservableResultUseCase` extend `UseCase`

--- core-lib-usecase/src/main/java/core/lib/usecase/ObservableResultUseCase.kt
+++ core-lib-usecase/src/main/java/core/lib/usecase/ObservableResultUseCase.kt
@@ -3,6 +3,6 @@ package core.lib.usecase
 import io.reactivex.Observable
 import core.lib.result.Result
 
-interface ObservableResultUseCase<Input, Output> {
-    operator fun invoke(input: Input): Observable<Result<Output>>
+interface ObservableResultUseCase<Input, Output> : UseCase<Input, Observable<Result<Output>>> {
+    override operator fun invoke(input: Input): Observable<Result<Output>>
 }
