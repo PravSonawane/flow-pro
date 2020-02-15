119eb986d260383d35dcfe72d7597b8c3c89d000
PravinSonawane <june.pravin@gmail.com>
2020-02-15 12:13:01 -0800

    Cleanup
    Signed-off-by: PravinSonawane <june.pravin@gmail.com>

--- ui-feature-create-new-flow/src/main/java/ui/newflow/title/NewFlowTitleFragment.kt
+++ ui-feature-create-new-flow/src/main/java/ui/newflow/title/NewFlowTitleFragment.kt
@@ -10,11 +10,10 @@ import androidx.navigation.fragment.findNavController
 import app.base.AppBaseFragment
 import core.lib.rxutils.plusAssign
 import domain.models.flow.Flow
-import ui.feature.create.newflow.databinding.FragmentNewFlowTitleBinding
 import io.reactivex.android.schedulers.AndroidSchedulers
 import ui.feature.create.newflow.R
+import ui.feature.create.newflow.databinding.FragmentNewFlowTitleBinding
 import ui.newflow.createstep.CreateStepFragment
-import ui.newflow.selectnode.NewFlowSelectNodeFragment
 import javax.inject.Inject
 
 class NewFlowTitleFragment : AppBaseFragment() {
