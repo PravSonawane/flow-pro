9f18372d623bab7bf4c6e7064c6690ceff8e3b62
PravinSonawane <june.pravin@gmail.com>
2020-02-23 19:23:42 -0800

    Remove single use const
    Signed-off-by: PravinSonawane <june.pravin@gmail.com>

--- ui-lib-views/src/main/java/ui/lib/views/CommonItemViewModels.kt
+++ ui-lib-views/src/main/java/ui/lib/views/CommonItemViewModels.kt
@@ -1,15 +1,8 @@
 package ui.lib.views
 
 import androidx.lifecycle.LiveData
-import androidx.recyclerview.widget.RecyclerView
 import ui.lib.utils.LiveDataFactory
 import ui.lib.utils.StreamFactory
-
-/** A [RecyclerView.Adapter] view type for [ItemListHeaderViewModel] */
-const val VIEW_TYPE_LIST_HEADER = 11001
-
-/** A [RecyclerView.Adapter] view type for [ItemListButtonViewModel] */
-const val VIEW_TYPE_LIST_BUTTON = 11002
 
 class ItemListHeaderViewModel(
     analyticsKey: String,
@@ -20,7 +13,7 @@ class ItemListHeaderViewModel(
     streamFactory,
     R.layout.item_list_header,
     BR.viewModel,
-    VIEW_TYPE_LIST_HEADER
+    11001
 ) {
 
     val headerText: LiveData<CharSequence> = liveDataFactory.liveData("18a333b8-d6a9")
@@ -38,7 +31,7 @@ class ItemListButtonViewModel(
     streamFactory,
     R.layout.item_list_button,
     BR.viewModel,
-    VIEW_TYPE_LIST_BUTTON
+    11002
 ) {
 
     val buttonText: LiveData<CharSequence> = liveDataFactory.liveData("17df35b8-486c")
