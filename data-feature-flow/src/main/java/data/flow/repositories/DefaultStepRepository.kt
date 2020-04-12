e9da331b18901ab8fa8973ea4d86e93893ff92ab
Pravin Sonawane <june.pravin@gmail.com>
2020-04-12 01:56:26 -0700

    Add empty lists where data is not found
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- data-feature-flow/src/main/java/data/flow/repositories/DefaultStepRepository.kt
+++ data-feature-flow/src/main/java/data/flow/repositories/DefaultStepRepository.kt
@@ -9,32 +9,38 @@ import io.reactivex.Observable
 class DefaultStepRepository : StepRepository {
 
     override fun getAll(flowId: String): Observable<Result<List<Step>>> {
-        return Observable.just(Result.OnSuccess(FakeStorage.getFlow(flowId).steps!!))
+        return Observable.just(Result.OnSuccess(FakeStorage.getFlow(flowId).steps ?: emptyList()))
     }
 
     override fun getCurrentInputSteps(stepId: String): Observable<Result<List<Step>>> {
         val step = FakeStorage.getAllSteps()
             .find { it.id == stepId }!!
-        return Observable.just(Result.OnSuccess(step.inputSteps!!))
+        return Observable.just(Result.OnSuccess(step.inputSteps ?: emptyList()))
     }
 
     override fun getCurrentOutputSteps(stepId: String): Observable<Result<List<Step>>> {
         val step = FakeStorage.getAllSteps()
             .find { it.id == stepId }!!
-        return Observable.just(Result.OnSuccess(step.outputSteps!!))
+        return Observable.just(Result.OnSuccess(step.outputSteps ?: emptyList()))
     }
 
     override fun getPossibleInputSteps(stepId: String): Observable<Result<List<Step>>> {
         val allSteps = FakeStorage.getAllSteps()
-        val inputSteps = FakeStorage.getAllSteps().find { it.id == stepId }!!.inputSteps!!.map { it.id }
-        val possibleInputSteps = allSteps.filter { it.id !in inputSteps }
+        val inputSteps = FakeStorage.getAllSteps()
+            .find { it.id == stepId }!!
+            .inputSteps ?: emptyList()
+        val inputStepIds = inputSteps.map { it.id }
+        val possibleInputSteps = allSteps.filter { it.id !in inputStepIds }
         return Observable.just(Result.OnSuccess(possibleInputSteps))
     }
 
     override fun getPossibleOutputSteps(stepId: String): Observable<Result<List<Step>>> {
         val allSteps = FakeStorage.getAllSteps()
-        val outputSteps = FakeStorage.getAllSteps().find { it.id == stepId }!!.outputSteps!!.map { it.id }
-        val possibleOutputSteps = allSteps.filter { it.id !in outputSteps }
+        val outputSteps = FakeStorage.getAllSteps()
+            .find { it.id == stepId }!!
+            .outputSteps ?: emptyList()
+        val outputStepIds = outputSteps.map { it.id }
+        val possibleOutputSteps = allSteps.filter { it.id !in outputStepIds }
         return Observable.just(Result.OnSuccess(possibleOutputSteps))
     }
 
